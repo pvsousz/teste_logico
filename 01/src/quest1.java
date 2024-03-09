b0af0c338fb74e63c845d577bf964155a44f8ea2
unknown <pauloviniciussous1@gmail.com>
2024-03-09 11:20:01 -0300

    teste_logica

---
+++ 01/src/quest1.java
@@ -0,0 +1,11 @@
+public class quest1 {
+    public static void main(String[] args) throws Exception {
+        int indice = 13, SOMA = 0 ,K =0;
+
+        while (K<indice) {
+            K = K +1;
+            SOMA = SOMA +K;
+        }
+        System.out.println("O valor da variável SOMA = "+ SOMA);
+    }   
+}
