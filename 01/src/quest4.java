d884aff005cb445f6dcd4b39784228d8c9012cd6
unknown <pauloviniciussous1@gmail.com>
2024-03-09 17:22:38 -0300

    Teste_Logico

--- 01/src/quest4.java
+++ 01/src/quest4.java
@@ -1,19 +1,19 @@
 public class quest4 {
     public static void main(String[] args) {
 
-        Ligo um interruptor 1
-        Desligo o interruptor 1 ligado e ligo um segundo interruptor
-        vou a uma sala e verifico se a luz está apagada e quente, se está ligada ou desligada
-        Se a luz estiver apagada e quente ou apagada e fria ou ligada 
-            Sei qual dos interuptores foi
-        Agora volto ligo um outro interruptor
-        Desligo esse mesmo interruptor
-        Ligo o terceiro interruptor
-        Vou até a sala 
-        Assim descubriei mais um interruptor correspondente a lâmpada
-        Por ultimo faltara a ultima lampada corresepondente ao interruptor 
+        // Ligo um interruptor 1
+        // Desligo o interruptor 1 ligado e ligo um segundo interruptor
+        // vou a uma sala e verifico se a luz está apagada e quente, se está ligada ou desligada
+        // Se a luz estiver apagada e quente ou apagada e fria ou ligada 
+        //     Sei qual dos interuptores foi
+        // Agora volto ligo um outro interruptor
+        // Desligo esse mesmo interruptor
+        // Ligo o terceiro interruptor
+        // Vou até a sala 
+        // Assim descubriei mais um interruptor correspondente a lâmpada
+        // Por ultimo faltara a ultima lampada corresepondente ao interruptor 
 
-        Finalizado
+        // Finalizado
     
 
     }
