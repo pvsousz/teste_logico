78c4a416820878f64a3c531c7ffcaa01e59c4fab
unknown <pauloviniciussous1@gmail.com>
2024-03-09 17:26:40 -0300

    Teste_logic

--- 01/src/quest4.java
+++ 01/src/quest4.java
@@ -5,11 +5,13 @@ public class quest4 {
         // Desligo o interruptor 1 ligado e ligo um segundo interruptor
         // vou a uma sala e verifico se a luz está apagada e quente, se está ligada ou desligada e fria
         // Se a luz estiver apagada e quente ou apagada e fria ou ligada 
-        //     Sei qual dos interuptores foi
+        //     Seja qual dos interruptores for
+        // Já saberei Um interruptor que corresponde a uma lâmpada assim falta 2
         // Agora volto ligo um outro interruptor
         // Desligo esse mesmo interruptor
         // Ligo o terceiro interruptor
         // Vou até a sala 
+        //Verifico se a luz está apagada e quente se está ligada ou desligada e fria
         // Assim descubriei mais um interruptor correspondente a lâmpada
         // Por ultimo faltara a ultima lampada corresepondente ao interruptor 
 
